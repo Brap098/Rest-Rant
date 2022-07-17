9b312f0ff3f87185b64976f28dcdbb27fdef2923
Brap098 <Brapagon.Work@gmail.com>
2022-07-16 21:51:27 -0400

    10 so far

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+  let comments = (
+    <h3 classname="inactive">
+      No comment yet!
+    </h3>
+  )
+  if (data.place.comments.length) {
+    comments = data.place.comments.map(c => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? 'Rant! 🤬' : 'Rave! 😎'}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <stong>- {c.author}</stong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      )
+    })
+  }
     return (
       <Def>
       <main>
@@ -10,6 +29,7 @@ function show (data) {
         <h2>Rating</h2>
         <p>Not Rated</p>
         <h2>Comments</h2>
+        {comments}
         <p>No comments yet!</p>
         <h3>
           Located in {data.place.city}, {data.place.state}
